83ed65f09adf55757214e2195e678b4a2b00cefc
sid.successive <siddharth@successivesoftwares.com>
2015-10-13 15:08:21 +0530

    API: Added API Routes

--- api_gateway/server.js
+++ api_gateway/server.js
@@ -15,6 +15,8 @@ app.use(express.static(path.join(__dirname, '../app')));
 app.use(express.static(path.join(__dirname, '../')));
 */
 
+
+
 var port = process.env.PORT || 9000;
 var router = express.Router();
 
@@ -23,7 +25,6 @@ router.use(function(req, res, next) {
   next();
 });
 
-
 router.get('/', function(req, res) {
   res.json({ message: 'Any Kind of get request' });
 });
@@ -31,9 +32,39 @@ router.get('/', function(req, res) {
 router.route('/test')
   .post(function(req, res) {
     res.json({ message: 'Post Testing Echo' });
-  });
+});
 
+// Multiple record (query 'kind' allowed)
+router.route('/:database/v2/:table')
+  .get(function(req, res) {
+    res.json({ message: 'Get/Database/V2/Table' });
+   })
+  .post(function(req, res) {
+    res.json({ message: 'Post/Database/V2/Table' });
+   })
+  .put(function(req, res) {
+    res.json({ message: 'Put/Database/V2/Table' });
+});
+
+// Single record by id
+router.route('/:database/v2/:table/:id')
+  .get(function(req, res) {
+    res.json({ message: 'Get/Database/V2/Table/Id' });
+  })
+  .patch(function(req, res) {
+    res.json({ message: 'Patch/Database/V2/Table/Id' });
+  })
+  .delete(function(req, res) {
+    res.json({ message: 'Delete/Database/V2/Table/Id' });
+});
+
+// Undelete a single deleted record
+router.route('/:database/v2/:table/:id/undelete')
+  .post(function(req, res) {
+    res.json({ message: 'Post/Database/V2/Table/Id/Undelete' });
+});
+
+module.exports = router;
 app.use('/api', router);
 app.listen(port);
 console.log('node server runing at  ' + port);
-
